8b14caffdeff87168c26b0d15796e789a6a562fc
kumar saurav <rkumar436032@gmail.com>
2026-02-04 11:47:19 +0530

    roman to integer

---
+++ Roman to Integer.py
@@ -0,0 +1,11 @@
+class Solution:
+    def romanToInt(self, s):
+        d = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        total = 0
+        prev = 0
+        for c in reversed(s):
+            val = d[c]
+            if val < prev: total -= val
+            else: total += val
+            prev = val
+        return total
